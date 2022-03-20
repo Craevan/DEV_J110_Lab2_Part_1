3951a6ba1e8d1251bc4a40c2bd5c2d4384ee8da2
a.siverskii <a.siverskii@gmail.com>
2022-03-20 18:14:45 +0300

    update Human class

--- src/ru/avalon/jdev/Human.java
+++ src/ru/avalon/jdev/Human.java
@@ -1,23 +1,30 @@
 package ru.avalon.jdev;
 
 public abstract class Human {
+
     private String firstName;
     private String lastName;
-    private Gender gender;
+    private final Gender gender;
     private String faculty;
 
-    public Human(final String firstName, final String lastName, final Gender gender, final String faculty) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Human(final String firstName,
+                 final String lastName,
+                 final Gender gender,
+                 final String faculty) {
+
+        setFirstName(firstName);
+        setLastName(lastName);
         this.gender = gender;
-        this.faculty = faculty;
+        setFaculty(faculty);
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(final String firstName) {
+        if (firstName == null)
+            throw new IllegalArgumentException("Ссылка на null");
         this.firstName = firstName;
     }
 
@@ -25,7 +32,9 @@ public abstract class Human {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(final String lastName) {
+        if (lastName == null)
+            throw new IllegalArgumentException("Ссылка на null");
         this.lastName = lastName;
     }
 
@@ -37,19 +46,31 @@ public abstract class Human {
         return faculty;
     }
 
-    public void setFaculty(String faculty) {
+    public void setFaculty(final String faculty) {
+        if (faculty == null)
+            throw new IllegalArgumentException("Ссылка на null");
         this.faculty = faculty;
     }
 
-    protected abstract void print();
+    protected void print() {
+        String info = "This is " + getFirstName() + " " + getLastName() +
+                ". " +
+                (getGender() == Gender.MALE ? Gender.MALE : Gender.FEMALE) +
+                " " + (this instanceof Teacher ? "teaches " : "studies ") +
+                "at " + getFaculty() + ".";
+        System.out.print(info);
+    }
 
-    public void printAll(Human[] humans) {
+    public static void printAll(final Human[] humans) {
+        if (humans == null)
+            throw new IllegalArgumentException("Ссылка на null");
         for (Human homo : humans)
             homo.print();
     }
 
     protected enum Gender {
-        MALE("He"), FEMALE("She");
+        MALE("He"), FEMALE("She"),
+        HIS("His"), HER("Her");
 
         private final String gender;
 
